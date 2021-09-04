0450122263a76e67f40779f031ac1341370214f0
Simón <offsivi@gmail.com>
2021-09-03 23:20:38 -0300

    based.copy fixed

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
 Import-Module posh-git
 Import-Module oh-my-posh
-Set-PoshPrompt -Theme ~/Documents/WindowsPowerShell/winmac.omp.json
-#Set-PoshPrompt -Theme ~/Documents/WindowsPowerShell/based.copy.omp.json
+#Set-PoshPrompt -Theme ~/Documents/WindowsPowerShell/winmac.omp.json
+Set-PoshPrompt -Theme ~/Documents/WindowsPowerShell/based.copy.omp.json
 #oh-my-posh --config sample.json --shell universal
